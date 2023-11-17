51c08cedfacbb99984724deb5b021b4322e500d8
Mamadou <125818224+mamadou186@users.noreply.github.com>
2023-11-17 16:05:25 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+const ratio = 0.1;
+const options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: ratio
+};
+
+const handleIntersect = function (entries, observer) {
+  entries.forEach(function (entry) {
+    if (entry.intersectionRatio > ratio) {
+      entry.target.classList.add('reveal-visible');
+    } else {
+      entry.target.classList.remove('reveal-visible');
+    }
+  });
+};
+
+document.documentElement.classList.add('reveal-loaded');
+
+window.addEventListener('DOMContentLoaded', function () {
+  const observer = new IntersectionObserver(handleIntersect, options);
+  document.querySelectorAll('.reveal').forEach(function (r) {
+    observer.observe(r);
+  });
+});
+
